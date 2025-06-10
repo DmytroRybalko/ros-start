b56e5244bbd027839c8f1ffc6856ae7441e43bce
Dmytro <mail4dmytro@example.com>
2025-06-10 20:52:39 +0000

    add hw1: number_publisher and number_counter

--- src/my_cpp_pkg/src/number_publisher.cpp
+++ src/my_cpp_pkg/src/number_publisher.cpp
@@ -4,26 +4,26 @@
 class NumberPublisherNode : public rclcpp::Node
 {
 public:
-    NumberPublisherNode() : Node("number_publisher"), robot_name_("N2P2")
+    NumberPublisherNode() : Node("number_publisher"), number_(2)
     {
-        publisher_ = this->create_publisher<example_interfaces::msg::Int64>("data", 10);
-        timer_ = this->create_wall_timer(
+        number_publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number", 10);
+        number_timer_ = this->create_wall_timer(
             std::chrono::seconds(1),
-            std::bind(&NumberPublisherNode::publish_data, this));
+            std::bind(&NumberPublisherNode::publishNumber, this));
         RCLCPP_INFO(this->get_logger(), "Number Publisher has been started");
     }
  
 private:
-    void publish_data()
+    void publishNumber()
     {
         auto msg = example_interfaces::msg::Int64();
-        msg.data = 42;
-        publisher_->publish(msg);
+        msg.data = number_;
+        number_publisher_->publish(msg);
     }
 
-    std::string robot_name_;
-    rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    int number_;
+    rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr number_publisher_;
+    rclcpp::TimerBase::SharedPtr number_timer_;
  
 };
  
